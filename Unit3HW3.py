c3181bf9ea6200f1ee3b21d37eb46c5b727af7ee
MateiEnache <146372728+MateiEnache@users.noreply.github.com>
2025-04-24 23:56:55 +0000

    aaa

--- Unit3HW3.py
+++ Unit3HW3.py
@@ -1,6 +1,8 @@
-x = input("Enter the first number: ")
-y = input("Enter the second number: ")
-try:
-    print(x+y)
-except ValueError:
-    print("Both mubers must be integers")
+while True:
+    x = input("Enter the first number: ")
+    y = input("Enter the second number: ")
+
+    try:
+        print(int(x) + int(y))
+    except ValueError:
+        print("Both inputs must be valid integers. Please try again.")
